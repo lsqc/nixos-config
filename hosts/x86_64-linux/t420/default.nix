abe93f8fd1f741f1c8c646f2ce9f1271c1934365
lsqc <lsqc@nya.vodka>
2026-07-01 21:14:06 +0200

    enable aarch64 emulation

--- hosts/x86_64-linux/t420/default.nix
+++ hosts/x86_64-linux/t420/default.nix
@@ -16,6 +16,7 @@
     ./printing.nix
     ./resolved.nix
     ./pcscd.nix
+    ./emulation.nix
   ];
 
   networking = {
---
+++ hosts/x86_64-linux/t420/emulation.nix
@@ -0,0 +1,5 @@
+{ ... }:
+
+{
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+}
