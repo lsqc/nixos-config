1304dc1ea81146fae113301ca48de1ce8a6a51cb
lsqc <lsqc@nya.vodka>
2026-07-01 21:27:06 +0200

    home package restructuring

--- home/lsqc/default.nix
+++ home/lsqc/default.nix
@@ -1,6 +1,4 @@
 {
-  inputs,
-  pkgs,
   ...
 }:
 
@@ -9,6 +7,7 @@
     ./desktop.nix
     ./modules
     ./host.nix
+    ./programs.nix
   ];
 
   home.username = "lsqc";
@@ -19,80 +18,4 @@
   programs.home-manager.enable = true;
 
   nixpkgs.config.allowUnfree = true;
-
-  home.packages = with pkgs; [
-    htop
-    playerctl
-
-    ipcalc
-    calc
-
-    # niri
-    swaybg
-    scrcpy
-
-    fuzzel
-    swaylock
-    hyprlock
-
-    prismlauncher
-    cargo
-    onefetch
-
-    cmake
-    speedtest-cli
-
-    timer
-    clock-rs
-
-    pngquant
-    xclip
-    scrot
-    betterlockscreen
-
-    playerctl
-    pamixer
-    vlc
-
-    scrcpy
-    openvpn
-
-    # dunst
-    libnotify
-
-    asciiquarium-transparent
-    feh
-    comma
-    wireguard-tools
-    clang
-    whois
-    killall
-    yt-dlp
-    usbutils
-    fzf
-    just
-    mtr
-    mv
-    brightnessctl
-    yazi
-    pandoc
-    opentofu
-    tldr
-    wl-mirror
-    wl-clipboard
-    wlr-randr
-    hyfetch
-
-    libreoffice-qt-fresh
-    arandr
-    texliveSmall
-
-    steam
-    jameica
-
-    xwayland-satellite
-    inputs.helium.packages.x86_64-linux.default
-
-    yubioath-flutter
-  ];
 }
---
+++ home/lsqc/programs.nix
@@ -0,0 +1,83 @@
+{
+  pkgs,
+  inputs,
+  ...
+}:
+
+{
+  home.packages = with pkgs; [
+    htop
+    playerctl
+
+    ipcalc
+    calc
+
+    # niri
+    swaybg
+    scrcpy
+
+    fuzzel
+    swaylock
+    hyprlock
+
+    prismlauncher
+    cargo
+    onefetch
+
+    cmake
+    speedtest-cli
+
+    timer
+    clock-rs
+
+    pngquant
+    xclip
+    scrot
+    betterlockscreen
+
+    playerctl
+    pamixer
+    vlc
+
+    scrcpy
+    openvpn
+
+    # dunst
+    libnotify
+
+    asciiquarium-transparent
+    feh
+    comma
+    wireguard-tools
+    clang
+    whois
+    killall
+    yt-dlp
+    usbutils
+    fzf
+    just
+    mtr
+    mv
+    brightnessctl
+    yazi
+    pandoc
+    opentofu
+    tldr
+    wl-mirror
+    wl-clipboard
+    wlr-randr
+    hyfetch
+
+    libreoffice-qt-fresh
+    arandr
+    texliveSmall
+
+    steam
+    jameica
+
+    xwayland-satellite
+    inputs.helium.packages.x86_64-linux.default
+
+    yubioath-flutter
+  ];
+}
