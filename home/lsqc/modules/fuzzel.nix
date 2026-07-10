72c8f0ff63412850dfbeded191375dac14af56f3
lsqc <lsqc@nya.vodka>
2026-07-10 11:33:18 +0200

    fuzzel config

--- home/lsqc/modules/fuzzel.nix
+++ home/lsqc/modules/fuzzel.nix
@@ -1,20 +1,30 @@
-{ config, ... }:
+{
+  config,
+  ...
+}:
 
 {
   programs.fuzzel = {
+
     enable = true;
 
     settings = {
       colors = {
         text = "ffffffff";
-        background = "000000cc";
+        background = "000000ee";
+        input = "e8fc03ff";
+        selection-text = "37faa2ff";
+        selection = "1f1f1fff";
+        selection-match = "00ff08ff";
       };
       border = {
         radius = 0;
       };
       main = {
+        enable-mouse = false;
+        use-bold = true;
+        placeholder = "...";
         font = "${config.theme.font.family}:size=${toString config.theme.font.size}";
-        # font = "Comic Shanns Mono Nerd Font:size=11";
       };
     };
   };
