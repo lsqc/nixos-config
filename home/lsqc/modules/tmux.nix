942817cba6c3574634e59080fdb4d2f444c9a838
lsqc <lsqc@nya.vodka>
2026-07-15 17:06:16 +0200

    ??

--- home/lsqc/modules/tmux.nix
+++ home/lsqc/modules/tmux.nix
@@ -10,37 +10,34 @@
     plugins = with pkgs; [ ];
 
     extraConfig = ''
-      setw -g mode-keys vi
-
-      # set prefix
-      unbind C-b
-      set-option -g prefix C-s
-      bind-key C-s send-prefix
-
-      # window splitting
-      unbind '"'
-      unbind %
-
-      bind | split-window -h -c "#{pane_current_path}"
-      bind - split-window -v -c "#{pane_current_path}"
-
-      bind h select-pane -L
-      bind j select-pane -D
-      bind k select-pane -U
-      bind l select-pane -R
-
-      set-window-option -g xterm-keys on
-
-      # theme
-      set -g pane-active-border-style 'fg=#E91E63'
-      set -g pane-border-style 'fg=#2C2C2C'
-
-      # status line
-      set -g status-style 'bg=#000000,fg=#3dd9a4'
-      set -g status-left-style 'bg=#000000,fg=#3dd9a4'
-      set -g status-right-style 'bg=#000000,fg=#3dd9a4'
-      set -g window-status-style 'bg=#000000,fg=#3dd9a4'
-      set -g window-status-current-style 'bg=#000000,fg=#3dd9a4'
+        setw -g mode-keys vi
+        # set prefix
+        unbind C-b
+        set-option -g prefix C-s
+        bind-key C-s send-prefix
+        # window splitting
+        unbind '"'
+        unbind %
+        bind | split-window -h -c "#{pane_current_path}"
+        bind - split-window -v -c "#{pane_current_path}"
+        bind -n M-h select-pane -L
+        bind -n M-j select-pane -D
+        bind -n M-k select-pane -U
+        bind -n M-l select-pane -R
+  #      bind h select-pane -L
+  #      bind j select-pane -D
+  #      bind k select-pane -U
+  #      bind l select-pane -R
+        set-window-option -g xterm-keys on
+        # theme
+        set -g pane-active-border-style 'fg=#E91E63'
+        set -g pane-border-style 'fg=#2C2C2C'
+        # status line
+        set -g status-style 'bg=#000000,fg=#3dd9a4'
+        set -g status-left-style 'bg=#000000,fg=#3dd9a4'
+        set -g status-right-style 'bg=#000000,fg=#3dd9a4'
+        set -g window-status-style 'bg=#000000,fg=#3dd9a4'
+        set -g window-status-current-style 'bg=#000000,fg=#3dd9a4'
     '';
   };
 }
