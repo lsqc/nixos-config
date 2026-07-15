49d56fb4f4460421a16e3f06001a20eec35f8b13
lsqc <lsqc@nya.vodka>
2026-07-15 14:16:04 +0200

    update firefox config

--- home/lsqc/modules/firefox.nix
+++ home/lsqc/modules/firefox.nix
@@ -32,6 +32,14 @@ in
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
           installation_mode = "force_installed";
         };
+        "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+        };
       };
     };
 
