dd90431645ebfa93958a8386218de40a24a237ac
lsqc <lsqc@nya.vodka>
2026-09-19 15:52:58 +0200

    terminal stuff

--- home/lsqc/modules/ghostty.nix
+++ home/lsqc/modules/ghostty.nix
@@ -8,10 +8,16 @@
       window-decoration = false;
 
       custom-shader = "${../../static/ghostty-shader.glsl}";
-      background = "#000000";
+
       font-size = config.theme.font.size;
       font-family = "${config.theme.font.family}";
+
       cursor-color = "#c516f5";
+      background = "#000000";
+
+      cursor-style = "block";
+      cursor-style-blink = true;
+      shell-integration-features = "no-cursor";
     };
   };
 }
