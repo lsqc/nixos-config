06d23f7a16a67866383dced21791524bbbabb9ed
lsqc <lsqc@nya.vodka>
2026-09-26 15:08:21 +0200

    nushell config

--- home/lsqc/modules/nu.nix
+++ home/lsqc/modules/nu.nix
@@ -3,6 +3,20 @@
   programs.nushell = {
     enable = true;
     shellAliases = config.programs.zsh.shellAliases;
+
+    extraConfig = ''
+      $env.PROMPT_INDICATOR = { ||
+          return ((ansi purple_bold) + " λ ")
+      }
+      $env.PROMPT_COMMAND = { ||
+          let dir = ((ansi cyan_bold) + (pwd | str replace $env.HOME "~") + (ansi reset))
+          return (" " + $dir + ((ansi --escape { fg: "#5f00ff"}) + ">" + (ansi reset)))
+      }
+
+      $env.config.table.mode = "compact"
+
+    '';
+
     settings = {
       show_banner = false;
       completions.external = {
